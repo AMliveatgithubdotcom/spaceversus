03b49ecac1116b5222c6100999e914af466450c1
AM <AM>
2021-04-28 11:45:13 +0200

    Second player, collision detection, simple health system

--- characters/characters.py
+++ characters/characters.py
@@ -8,21 +8,24 @@ class player(object):
         self.width = 64
         self.height = 64
         self.vel = 5
+        self.radius = 30
         self.maxspeed = 10
         self.acceleration = 0.1
         self.deceleration = 0.01
+        self.health = 100
     def update(self, screen, angle, x, y):
         newimage = pygame.transform.rotate(self.icon, angle * -1)
         screen.blit(newimage, (x - int(newimage.get_width() / 2) , y - int(newimage.get_height() / 2) % 720))
 
 class projectile(object):
-    def __init__(self, x, y, cos, sin):
-        self.icon = pygame.image.load('characters/blast.png').convert_alpha()
+    def __init__(self, x, y, cos, sin, starttime):
         self.x = x
         self.y = y
         self.traveltime = 0
         self.cos = cos
         self.sin = sin
+        self.radius = 5
         self.velocity = 20
-    def draw(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), 5)
+        self.starttime = starttime
+    def draw(self, screen, color):
+        pygame.draw.circle(screen, color, (self.x, self.y), self.radius)
--- main.py
+++ main.py
@@ -15,28 +15,45 @@ class game():
         screeny = 720
         screen = pygame.display.set_mode((screenx, screeny))
         debugfont = pygame.font.SysFont(None, 24)
-        projectiles = []
+        projectilesp1 = []
+        projectilesp2 = []
         player1 = characters.player(screenx / 2, screeny / 2)
-        p1currentvelx, p1currentvely = 0, 0
+        player2 = characters.player(screenx / 4, screeny / 4)
+        p1currentvelx, p1currentvely, p2currentvelx, p2currentvely = 0, 0, 0, 0
         running = True
         p1rotation = -90
+        p2rotation = -90
         p1throttling = False
+        p2throttling = False
         debugmode = False   
 
-        # ---Main loop. This is where moving the ships and reading button presses is handled. --- #
+        # --- Main loop. This is where moving the ships and reading button presses is handled. --- #
         while running:
         # End the game #
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-            # ---Controls, calculation of velocity and acceleration--- #
+                if event.type == pygame.KEYDOWN:
+                    # Projectiles #
+                    if event.key == pygame.K_SPACE:
+                        projectilesp1.append(characters.projectile(player1.x, player1.y, math.cos(p1rotation * math.pi / 180), math.sin(p1rotation * math.pi / 180), pygame.time.get_ticks()))
+                    if event.key == pygame.K_KP_0:
+                        projectilesp2.append(characters.projectile(player2.x, player2.y, math.cos(p2rotation * math.pi / 180), math.sin(p2rotation * math.pi / 180), pygame.time.get_ticks()))
+                    # Debug enable #
+                    if event.key == pygame.K_F5:
+                        debugmode = not debugmode
+            # --- Controls, calculation of velocity and acceleration--- #
             # Rotation #
-            if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            if pygame.key.get_pressed()[pygame.K_d]:
                 p1rotation += 5
-            if pygame.key.get_pressed()[pygame.K_LEFT]:
+            if pygame.key.get_pressed()[pygame.K_a]:
                 p1rotation -= 5
+            if pygame.key.get_pressed()[pygame.K_KP_6]:
+                p2rotation += 5
+            if pygame.key.get_pressed()[pygame.K_KP_4]:
+                p2rotation -= 5
             # Enable the ship throttle and accelerate. #
-            if pygame.key.get_pressed()[pygame.K_UP]:
+            if pygame.key.get_pressed()[pygame.K_w]:
                 p1tempvelx = p1currentvelx + player1.acceleration * math.cos(p1rotation * math.pi / 180)
                 p1tempvely = p1currentvely + player1.acceleration * math.sin(p1rotation * math.pi / 180)
                 if player1.maxspeed*-1 < p1tempvelx < player1.maxspeed:
@@ -44,24 +61,31 @@ class game():
                 if player1.maxspeed*-1 < p1tempvely < player1.maxspeed:
                     p1currentvely = round(p1tempvely, 2)
                 p1throttling = True
-            # Projectiles. #
-            if pygame.key.get_pressed()[pygame.K_SPACE]:
-                projectiles.append(characters.projectile(player1.x, player1.y, math.cos(p1rotation * math.pi / 180), math.sin(p1rotation * math.pi / 180)))
-            # Switch to debug #
-            if pygame.key.get_pressed()[pygame.K_F5]:
-                debugmode = not debugmode
+
+            if pygame.key.get_pressed()[pygame.K_KP_8]:
+                p2tempvelx = p2currentvelx + player2.acceleration * math.cos(p2rotation * math.pi / 180)
+                p2tempvely = p2currentvely + player2.acceleration * math.sin(p2rotation * math.pi / 180)
+                if player2.maxspeed*-1 < p2tempvelx < player2.maxspeed:
+                    p2currentvelx = round(p2tempvelx, 2)
+                if player2.maxspeed*-1 < p2tempvely < player2.maxspeed:
+                    p2currentvely = round(p2tempvely, 2)
+                p2throttling = True
             # Check if the ship is throttled. If not, decelerate it slowly. #
-            # TODO: Round the final result? Movement feels weird. #
             if p1throttling == 0:
                 if p1currentvelx != 0:
                     p1currentvelx = round(p1currentvelx -copysign(player1.deceleration, p1currentvelx), 2)
                 if p1currentvely != 0:
                     p1currentvely = round(p1currentvely -copysign(player1.deceleration, p1currentvely), 2)
+            if p2throttling == 0:
+                if p2currentvelx != 0:
+                    p2currentvelx = round(p2currentvelx -copysign(player2.deceleration, p2currentvelx), 2)
+                if p2currentvely != 0:
+                    p2currentvely = round(p2currentvely -copysign(player2.deceleration, p2currentvely), 2)
             # Set the throttle back to 0 after every check has been completed in one cycle. #
-            p1throttling = False
+            p1throttling, p2throttling = False, False
             # Move the ship by changing its coordinates. Speed and direction gets calculated when the UP button is pressed. #
-            player1.x = (player1.x + p1currentvelx) % screenx
-            player1.y = (player1.y + p1currentvely) % screeny
+            player1.x, player1.y = (player1.x + p1currentvelx) % screenx, (player1.y + p1currentvely) % screeny
+            player2.x, player2.y = (player2.x + p2currentvelx) % screenx, (player2.y + p2currentvely) % screeny
             # Redraw the ships at a new position. #
             screen.fill((50,50,50))
             # Debug Mode #
@@ -74,18 +98,38 @@ class game():
                 screen.blit(fps, (64 , 92))
             # Debug Mode End #
             # Process every projectile, move them or delete them if their traveltime has reached the limit. #
-            for projectile in projectiles:
-                if projectile.traveltime < 500:
-                    poldx = projectile.x
-                    poldy = projectile.y
+            # Player 1 #
+            for projectile in projectilesp1:
+                if projectile.traveltime < 1800:
                     projectile.x = (projectile.x + (10 * projectile.cos)) % screenx
                     projectile.y = (projectile.y + (10 * projectile.sin)) % screeny
-                    projectile.traveltime += math.sqrt((projectile.x - poldx) ** 2 + (projectile.y - poldy) ** 2)
-                    projectile.draw(screen)
+                    projectile.traveltime = pygame.time.get_ticks() - projectile.starttime
+                    projectile.draw(screen, (255, 0, 0))
+                    if math.sqrt((max(projectile.x, player2.x) - min(projectile.x, player2.x))**2 + (max(projectile.y, player2.y) - min(projectile.y, player2.y))**2) < player2.radius + projectile.radius:
+                        player2.health -= 10
+                        projectilesp1.pop(projectilesp1.index(projectile))
                 else: 
-                    projectiles.pop(projectiles.index(projectile))
+                    projectilesp1.pop(projectilesp1.index(projectile))
+            # Player 2 #
+            for projectile in projectilesp2:
+                if projectile.traveltime < 1800:
+                    projectile.x = (projectile.x + (10 * projectile.cos)) % screenx
+                    projectile.y = (projectile.y + (10 * projectile.sin)) % screeny
+                    projectile.traveltime = pygame.time.get_ticks() - projectile.starttime
+                    projectile.draw(screen, (0, 0, 255))
+                    if math.sqrt((max(projectile.x, player1.x) - min(projectile.x, player1.x))**2 + (max(projectile.y, player1.y) - min(projectile.y, player1.y))**2) < player1.radius + projectile.radius:
+                        player1.health -= 10
+                        projectilesp2.pop(projectilesp2.index(projectile))
+                else:
+                    projectilesp2.pop(projectilesp2.index(projectile))
+            # End of Projectiles #
+
+            # Game Over Conditions #
+            if (player2.health or player1.health) <= 0:
+                pygame.QUIT
             # Redraw player.
             player1.update(screen, p1rotation, player1.x % screenx, player1.y % screeny)
+            player2.update(screen, p2rotation, player2.x % screenx, player2.y % screeny)
             pygame.display.flip()
             clock.tick(60)
 
